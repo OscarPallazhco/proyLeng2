6bc3560577c834e811ddc1af225d4c8df3dc36a7
OscarPallazhco <edduard.96@gmail.com>
2019-08-12 11:13:24 -0500

    Add files via upload
    problema cuando la lista de user_mentions está vacía.

--- main.py
+++ main.py
@@ -12,26 +12,27 @@ def cargarInfoArchivo(cuenta):
 
     contador = 1
     for tweet in datos_json["tweets"]:
-        usuario = "@" + str(tweet["entities"]["user_mentions"][0]["screen_name"])
-        nombre = str(tweet["entities"]["user_mentions"][0]["name"])
-        l_hashtags = tweet["entities"]["hashtags"] #lista de diccionarios
-        hashtags = []
-        for elemento in l_hashtags:
-            hashtags.append(elemento["text"])
+        if len(tweet["entities"]["user_mentions"]) !=0:
+            usuario = "@" + str(tweet["entities"]["user_mentions"][0]["screen_name"])
+            nombre = str(tweet["entities"]["user_mentions"][0]["name"])
+            l_hashtags = tweet["entities"]["hashtags"] #lista de diccionarios
+            hashtags = []
+            for elemento in l_hashtags:
+                hashtags.append(elemento["text"])
 
-        print(str(contador)+". Usuario:",nombre,"(",usuario,")")
-        print("\tFecha:",tweet["created_at"])
-        print("\tMensaje:",end="")
-        if ("retweeted_status" in tweet):
-            print(tweet["retweeted_status"]["text"])
-        else:
-            print(tweet["text"])
-        print("\tHahtags:",hashtags)
-        contador+=1
-        print()
+            print(str(contador)+". Usuario:",nombre,"(",usuario,")")
+            print("\tFecha:",tweet["created_at"])
+            print("\tMensaje:",end="")
+            if ("retweeted_status" in tweet):
+                print(tweet["retweeted_status"]["text"])
+            else:
+                print(tweet["text"])
+            print("\tHahtags:",hashtags)
+            contador+=1
+            print()
 
 
 cuentas = ["@IESSec","@SRIoficialEc","@CPCCS"]
-cuenta = cuentas[2]
+cuenta = cuentas[0]
 print("*"*50,cuenta,"*"*50)
 cargarInfoArchivo(cuenta)
